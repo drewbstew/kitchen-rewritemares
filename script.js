d2a7097e92db94cdf9f8c4a5e5c671e7582f8aec
Drew Stewart <drew.b.stew@gmail.com>
2017-05-04 19:51:57 -0700

    added bones, func to change ep title

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-// VARIABLES
+// SELECTORS
 
 var chefFirstName = $('#chef-first-name')
 var chefLastName = $('#chef-last-name')
-var restaurantName = $('#restaurant-name') 
+var restaurantName = $('#restaurant-name')
+var submitButton = $('#submit')
+// DOM
+
+var episodeTitle = $('#episode-title')
+
+// EVENTS
+
+submitButton.on('click', function(){
+  changeEpisodeTitle();
+})
+
+//LOGIC
+
+//METHODS
+
+function changeEpisodeTitle(name){
+  episodeTitle.text(
+    'Episode: ' + restaurantName.val()
+  )
+}
+
+/*
+DOCUMENT READY
+*/
+
+$(document).ready(function() {
+
+});
